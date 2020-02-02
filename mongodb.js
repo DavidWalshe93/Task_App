3911d065db29a219c7956983c0a7bc83059e2676
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-02-02 21:49:49 +0000

    Added code to insert multiple documents to a collection in mongodb.js.

--- mongodb.js
+++ mongodb.js
@@ -14,10 +14,31 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     // Get database handle
     const db = client.db(databaseName);
 
-    // Add a document to a collection
-    db.collection("users").insertOne({
-        name: "Dave",
-        age: 26
+    // // Add a document to a collection
+    // db.collection("users").insertOne({
+    //     name: "Dave",
+    //     age: 26
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log("Unable to insert user")
+    //     }
+    //     console.log(result.ops)
+    // });
+
+    db.collection("users", ).insertMany([
+        {
+            name: "Ryan",
+            age: 21
+        }, {
+            name: "Grace",
+            age: 26
+        }
+        ], (error, result) => {
+        if (error) {
+            return console.log(result.ops);
+        }
+
+        console.log(result.ops)
     })
 });
 
