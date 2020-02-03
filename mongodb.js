1a91d7b0775b56e3dfedce50b955eeff047fe2ec
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-02-03 22:04:24 +0000

    Used $set operator in a updateOne command for mongodb to update a document.

--- mongodb.js
+++ mongodb.js
@@ -11,31 +11,16 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     // Get database handle
     const db = client.db(databaseName);
 
-    db.collection("users").findOne({ _id: new ObjectID("5e3743d766fefc193c4c863e")}, (error, user) => {
-        if (error) {
-            return console.log("Unable to fetch")
-        }
-
-        console.log(user)
-    });
-
-    // Find and print all the documents in user where the age is 27.
-    db.collection("users").find({ age: 26 }).toArray((error, users) => {
-        console.log(users)
-
-    });
-
-    // Find how many documents match the age of 26 in the users collection.
-    db.collection("users").find({ age: 26 }).count((error, count) => {
-        console.log(count)
-    });
-
-    db.collection("tasks").findOne({_id: new ObjectID("5e37449b4e7f154a00c9a1ac")}, (error, task) => {
-        console.log(task)
-    });
-
-    db.collection("tasks").find({completed: false}).toArray((error, tasks) => {
-        console.log(tasks)
-    });
+    db.collection("users").updateOne({
+        _id: new ObjectID("5e3741433cf32837842e1249")
+    }, {
+       $set: {
+           age: 27
+       }
+    }).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error)
+    })
 });
 
