6e22e45094a46896024bb4d300e549c4622d670c
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-02-03 23:09:08 +0000

    Added mongoose task model requirements.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
     userNewUrlParser: true,
@@ -7,16 +8,35 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 
 const User = mongoose.model("User", {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                    throw new Error("Email is not valid")
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error("Age must be a positive number")
+            }
+        }
     }
 });
 
 const me = new User({
-    name: "Dave",
-    age: 27
+    name: "    Tom    ",
+    email: "TOM@TEST.COM   "
 });
 
 me.save().then(() => {
@@ -25,22 +45,23 @@ me.save().then(() => {
     console.log("ERROR: ", error)
 });
 
-const Task = mongoose.model("Task", {
-    description:  {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    }
-});
-
-const task = new Task({
-    description: "This is a task",
-    completed: true
-});
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log(error)
-});
+// const Task = mongoose.model("Task", {
+//     description:  {
+//         type: String,
+//
+//     },
+//     completed: {
+//         type: Boolean
+//     }
+// });
+//
+// const task = new Task({
+//     description: "This is a task",
+//     completed: true
+// });
+//
+// task.save().then(() => {
+//     console.log(task)
+// }).catch((error) => {
+//     console.log(error)
+// });
