d7acbd0cfd3aacd2d784d0d9e7ec5cf16bee0501
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-02-05 23:35:42 +0000

    Added endpoint to logout all sessions related to a user.

--- src/routers/users.js
+++ src/routers/users.js
@@ -49,6 +49,17 @@ router.post("/users/logout", auth, async (req, res) => {
     }
 });
 
+// // Logout all users
+router.post("/users/logoutAll", auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+});
+
 // Retrieve all Users
 router.get("/users/me", auth, async (req, res) => {
     res.send(req.user)
