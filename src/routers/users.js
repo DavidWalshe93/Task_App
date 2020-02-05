b89987c2de35cfcc59f2c6a9f2619d1ee746271b
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-02-05 23:42:52 +0000

    Removed the endpoint to retrieve a user by id, users should only be able to retrieve information relating to themselves.

--- src/routers/users.js
+++ src/routers/users.js
@@ -66,22 +66,6 @@ router.get("/users/me", auth, async (req, res) => {
 });
 
 
-// Retrieve a User by ID
-router.get("/users/:id", async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user);
-    } catch (e) {
-        res.status(500).send(e)
-    }
-});
-
 // Update data for a user.
 router.patch("/users/:id", async (req, res) => {
     const updates = Object.keys(req.body);
