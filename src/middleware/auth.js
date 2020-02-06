401bcd24f4265348f7e331461f9c9197bb536501
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-02-06 00:54:02 +0000

    Lab 12 complete.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,6 @@ const User = require(("../model/user"));
 
 const auth = async (req, res, next) => {
     try {
-        console.log("HELLOR")
         const token = req.header("Authorization").replace("Bearer ", "");
         const decoded = jwt.verify(token, "MY_SECRET");
         const user = await User.findOne({_id: decoded._id, "tokens.token": token});
