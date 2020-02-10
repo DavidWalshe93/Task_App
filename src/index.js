96171861073e30e6a291c62601e9915ab3de20f2
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-02-10 01:51:31 +0000

    Fixed crash issue in index.js.

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ const taskRouter = require("./routers/tasks");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-});
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
